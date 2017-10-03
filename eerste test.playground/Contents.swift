3ce5df8cc7b98aa6c75a9014f6f6839d4a7f9393
TVke <thomas.verhelst@student.kdg.be>
2017-10-03 14:37:34 +0200

    initial commit

---
+++ eerste test.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: A UIKit based Playground for presenting user interface
+  
+//import UIKit
+//import PlaygroundSupport
+//
+//class MyViewController : UIViewController {
+//    override func loadView() {
+//        let view = UIView()
+//        view.backgroundColor = .white
+//
+//        let label = UILabel()
+//        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+//        label.text = "Hello World!"
+//        label.textColor = .black
+//
+//        view.addSubview(label)
+//        self.view = view
+//	}
+//}
+// Present the view controller in the Live View window
+//PlaygroundPage.current.liveView = MyViewController()
+
+let test = 2
+var tester = 5
+
+let eerst = "Test is \(test) en tester is \(test+tester)"
+
+let listOfNumbres = [14,17,75,32,99]
+
+for number in listOfNumbres{
+	if number > 20{
+		print("klein\n")
+	}else{
+		print("groot\n")
+	}
+}
+
