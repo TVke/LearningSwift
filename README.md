d4f47589c52573473708a92e63118f8bd0ed37f1
TVke <thomas.verhelst@student.kdg.be>
2017-11-16 19:51:37 +0100

    clean up of project folder

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Me teaching myself Swift
 
 1 testing, exploring examples from "The Swift Programming Language
+2 watch a lot of videos
+3 setting up stuff to show
--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -1,74 +1,36 @@
-//: A UIKit based Playground for presenting user interface
-
-//import UIKit
-//import PlaygroundSupport
-//
-//class MyViewController : UIViewController {
-//    override func loadView() {
-//        let view = UIView()
-//        view.backgroundColor = .white
-//
-//        let label = UILabel()
-//        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-//        label.text = "Hallo Word!"
-//        label.textColor = .black
-//
-//        view.addSubview(label)
-//        self.view = view
-//	}
-//}
-//
-////Present the view controller in the Live View window
-//PlaygroundPage.current.liveView = MyViewController()
-
 // code explorations
+// variable
 
 let test = 2
+//test = 1 // not possible to change a constant
 var tester = 15
 
-let eerst = "Test is \(test) en tester is \(test+tester)"
+let eerst = "tester + test is \(test+tester)"
+type(of: eerst)
 
-let listOfNumbres = [14,17,75,32,99]
+var number = 5.7
+type(of: number)
 
-for number in listOfNumbres{
-	if number > 20{
-		print("klein\n")
-	}else{
-		print("groot\n")
-	}
-}
+var floatDef:Float = 2.9
+var doubleDef:Double = 2.9
 
-let interestingNumbers = [
-	"maten":[2,3,14,16],
-	"makkers":[4,5,7,9],
-]
 
-var largestNumber = 0
+// switch (super cool)
 
-for (names,numbers) in interestingNumbers{
-	print("names")
-	for number in numbers{
-		if number > largestNumber{
-			largestNumber = number
-		}
-	}
-}
-print(largestNumber)
-
-//test = 1
 tester = 5
 
 switch tester {
-case 5:
+case 0,1,2,3,4,5:
 	print("I am between 0 and 5")
-case 10:
+case 6...10:
 	print("I am between 5 and 10")
 case 15:
-	print("I am between 10 and 15")
+	print("I am 15")
 default:
 	print("I am none of the ones before me")
 }
 
+// switch when needed
 tester = 1
 
 switch tester {
@@ -82,6 +44,70 @@ case 5:
 default:
 	print("something went wrong")
 }
+
+// looping
+
+// for in
+let listOfNumbres = [14,17,75,32,99]
+
+for number in listOfNumbres{
+	if number > 20{
+		print("klein\n")
+	}else{
+		print("groot\n")
+	}
+}
+
+let interestingNumbers = [
+	"maten":[162,183,194,165],
+	"makkers":[149,153,174,190],
+]
+
+var largestNumber = 0
+
+for (names,numbers) in interestingNumbers{
+	print(names)
+	print(numbers)
+	for number in numbers{
+		if number > largestNumber{
+			largestNumber = number
+		}
+	}
+}
+print(largestNumber)
+
+// while
+
+var give = 3
+while give < 100 {
+	give *= 2 // watch quicklook
+}
+
+print(give)
+
+
+//using UIKit to show applike stuff
+
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hallo Word!"
+        label.textColor = .black
+
+        view.addSubview(label)
+        self.view = view
+	}
+}
+
+//Presents MyViewController in the Live View window to test app stuff
+PlaygroundPage.current.liveView = MyViewController()
 
 
 
